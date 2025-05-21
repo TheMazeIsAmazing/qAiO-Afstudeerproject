7f08df197b871dfdd4f2c545d989ab50c5398608
mees.muller <mees-muller@proton.me>
2025-05-21 12:53:46 +0200

    Reverting to codebase prototype 1

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-import flask
 from flask import Flask, render_template, request
-from openai import OpenAI, OpenAIError
-import json, base64, os
-from pathlib import Path
+from openai import OpenAI
+import json, os
 
 client = OpenAI(
     base_url="https://api.bonzai.iodigital.com/universal",
@@ -34,15 +32,6 @@ def home():
         prev_message_str = request.form.get('prev_message', '[]')
         message = request.form.get('message')
 
-        print(request.files.get('files'))
-
-        for uploaded_file in request.files.getlist('files'):
-            if uploaded_file.filename != '':
-                client.files.create(
-                    file=uploaded_file.read(),
-                    purpose="user_data",
-                )
-
         # Parse previous messages from JSON
         try:
             prev_message = json.loads(prev_message_str)
@@ -60,21 +49,17 @@ def home():
             role = "user" if i % 2 == 0 else "assistant"
             conversation.append({"role": role, "content": msg})
 
-
         # Add the new user message
         conversation.append({"role": "user", "content": message})
 
         print(f"conversation: {conversation}")
 
-        try:
-            # Call the API with the full conversation history
-            completion = client.chat.completions.create(
-                model="claude-3-5-sonnet",
-                messages=conversation,
-                # temperature=0.0 # ToDo: Test different temperatures with QA'ers
-            )
-        except OpenAIError as e:
-            return render_template('oi.html', error=e)
+        # Call the API with the full conversation history
+        completion = client.chat.completions.create(
+            model="claude-3-7-sonnet",
+            messages=conversation,
+            # temperature=0.0 # ToDo: Test different temperatures with QA'ers
+        )
 
         chat_message = completion.choices[0].message.content
 
@@ -92,10 +77,6 @@ def home():
     else:
         return render_template('home.html', chat_messages=[])
 
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('oi.html', error="404 - Niet gevonden", hide_btn=True), 404
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
